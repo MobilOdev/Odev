2ace4f4437278568eed85326c649c46b30af04c7
Orhun Özdemir <orhun.ozdemir@hotmail.com>
2020-12-26 23:26:58 +0300

    loginpage design begin

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , TextInput} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { color } from 'react-native-reanimated';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Green Plant</Text>
+      
+      <TextInput placeholder={"Enter User Name"}style={{width:"70%",height:"5%",borderBottomWidth:1}}></TextInput>
+      <TextInput placeholder={"Enter Password"}style={{width:"70%",height:"5%",borderBottomWidth:1,margin:10}}></TextInput>
+      <TouchableOpacity style={{height:43,width:200,backgroundColor:"black",alignContent:"center",alignItems:"center",borderRadius:40,justifyContent:"center",marginTop:"5%"}}>
+      <Text style={{color:"white",}}>Login</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +24,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    alignContent:"center",
   },
 });
